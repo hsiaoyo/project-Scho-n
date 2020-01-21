e67826734a2ed1d8039f9b10ff9d3730e9685f4a
yuhsuan <snowsharon18@gmail.com>
2020-01-21 21:45:40 +0800

    owl-carousel

--- js/home.js
+++ js/home.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
   $('.user-tool .burger').click(function(e){
     $(e.currentTarget).parent().siblings('ul').toggleClass('display-block');
   });
+  $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:4
+        }
+    }
+})
 
 
 })
