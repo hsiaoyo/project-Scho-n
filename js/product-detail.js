00930281eb888267f147efaf654e3091e6094047
yuhsuan <snowsharon18@gmail.com>
2020-02-16 15:30:16 +0800

    thumbs carousel js

--- js/product-detail.js
+++ js/product-detail.js
@@ -6,12 +6,23 @@ $(document).ready(function(){
     nav:true,
     responsive:{
         0:{
-            items:3
+            items:1
         },
-        600:{
-            items:4
-        }
-     }
+
+     },
+    thumbs: true,
+
+    // When only using images in your slide (like the demo) use this option to dynamicly create thumbnails without using the attribute data-thumb.
+    thumbImage: false,
+
+    // Enable this if you have pre-rendered thumbnails in your html instead of letting this plugin generate them. This is recommended as it will prevent FOUC
+    thumbsPrerendered: true,
+
+    // Class that will be used on the thumbnail container
+    thumbContainerClass: 'owl-thumbs',
+
+    // Class that will be used on the thumbnail item's
+    thumbItemClass: 'owl-thumb-item'
   });
 
   $('.tabs li').click(function(e){
