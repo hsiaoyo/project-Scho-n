6ad9e165c477002e3feb832780ff3ac507fd1e8c
yuhsuan <snowsharon18@gmail.com>
2020-02-22 17:37:51 +0800

    delete modal's related  js

--- js/main.js
+++ js/main.js
@@ -20,19 +20,15 @@ $(document).ready(function(){
     $(this).siblings().find('.navbar').removeClass('display-block');
     $(this).siblings().find('.dropdown-toggle').siblings().removeClass('display-block');
   });
-  $('.modal').click(function(e){
-    $(this).removeClass('display-block')
-    $(this).siblings('div').removeClass('display-block');
-  });
+
   $('.dropdown-toggle').click(function(e){
-    $(this).siblings('.modal').addClass('display-block');
     $(this).closest('.dropdown').siblings('.dropdown').find('.dropdown-toggle').siblings().hide();
     $(this).siblings('.dropdown-menu').toggleClass('display-block');
     $(this).siblings('.triangle.pd').toggleClass('display-block');
   });
   $('.dropdown-item h3').click(function(e){
-    $(this).parents('.dropdown-item').siblings().find('.category').slideUp();
-    $(this).siblings('.category').slideToggle();
+    $(this).parents('.dropdown-item').siblings().find('.category').hide();
+    $(this).siblings('.category').toggle();
   })
   $('#cart').on('click', '.remove', function(e){
     var result = confirm('You are deleting a product from your cart');
